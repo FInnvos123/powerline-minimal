b65e9807171a7a4c320e331f44f6f55d9feec3c6
Finn Vos <finnvos34@protonmail.ch>
2018-11-02 15:18:52 +0100

    Display git branch name

--- powerline-minmal.zsh
+++ powerline-minmal.zsh
@@ -9,6 +9,22 @@ print-user() {
     echo -n " %F{240}%n $reset_color$separator_char"
 }
 
+print-git-status() {
+    local branch=$(git symbolic-ref --short HEAD 2> /dev/null)
+
+    if [[ -n "$branch" ]]; then
+        git diff --quiet --ignore-submodules --exit-code HEAD > /dev/null 2>&1
+
+        if [[ "$?" != 0 ]]; then
+            text_color=red
+        else
+            text_color=blue
+        fi
+
+        echo -n "%F{$text_color} $branch $reset_color$separator_char"
+    fi
+}
+
 print-dir() {
     echo -n " %~ $separator_char"
 }
@@ -16,6 +32,7 @@ print-dir() {
 prompt() {
     print-last-code
     print-user
+    print-git-status
     print-dir
     echo -n " "
 }
